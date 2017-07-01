1150fe100211cc4ac48f51de644a5c2a44ce599c
Rajat Gupta <rjtgupta84@gmail.com>
2017-07-01 17:07:45 +0530

    Revert "Add microG packages"
    This reverts commit 96696e6d389a69af4c6c1f8d035339e34e6ef774.
    
    Enable manual installation. (Unsigned APKs not building)

--- device.mk
+++ device.mk
@@ -283,8 +283,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-# microG Packages
-PRODUCT_PACKAGES += \
-   GmsCore \
-   GsfProxy \
-   FakeStore
