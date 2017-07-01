463df95b80ceda38d6b3e71adcff01a3b5f52d5d
Rajat Gupta <rjtgupta84@gmail.com>
2017-07-01 17:16:47 +0530

    Fix for broken navbar && missing Developer Options

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,6 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fh_athene.mk
+# Inherit some stuff from here:
+ $(LOCAL_DIR)/fh_athene.mk
