e7ac6f09a9992a42be60408bfd0b70c863b16c37
Nikit Sharma <nikitsharma92@gmail.com>
2017-11-23 14:08:09 +0530

    athene: Initial changes for PixelExperience

--- aosp_athene.mk
+++ aosp_athene.mk
@@ -14,7 +14,9 @@
 # limitations under the License.
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_ARCH := arm
+TARGET_DENSITY := xxhdpi
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
@@ -25,7 +27,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := lineage_athene
+PRODUCT_NAME := aosp_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := athene
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo aosp_athene-userdebug
+add_lunch_combo aosp_athene-user
